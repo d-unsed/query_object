017c0faffc4663ad78bdd227d81c8f63f1547c54
Dmitry Gritsay <unseductable@gmail.com>
2014-11-04 08:55:13 +0300

    Allow QueryObject#initialize to receive a block

--- lib/query_object/query_object.rb
+++ lib/query_object/query_object.rb
@@ -2,8 +2,8 @@ module QueryObject
   class QueryObject
     attr_reader :query
 
-    def initialize(query)
-      @query = query
+    def initialize(query = nil, &block)
+      @query = block_given? ? block : -> { query }
     end
   end
 end
--- spec/query_object/query_object_spec.rb
+++ spec/query_object/query_object_spec.rb
@@ -6,6 +6,18 @@ describe QueryObject::QueryObject do
   subject(:query_object) { described_class.new(query) }
 
   describe '#initialize' do
-    its(:query) { is_expected.to eq(query) }
+    context 'when query is a relation' do
+      its(:query) { is_expected.to be_instance_of(Proc) }
+    end
+
+    context 'when query is a block' do
+      subject do
+        described_class.new { query }
+      end
+
+      it 'does not wrap the block to a proc again' do
+        expect(subject.query.call).to eq(query)
+      end
+    end
   end
 end
